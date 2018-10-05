c2f5852f7de24bca314d42daaabad75f78ff4a98
Prashant  Bagdia <prashant.optum@gmail.com>
2018-10-06 01:13:03 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting who might have survived the titanic disaster',
+    author='Prashant',
+    license='MIT',
+)
